2de3e87895a3e3a9b7f263507e496d3b59c1707d
linusmbae <linusmutugi5178@gmail.com>
2020-08-17 14:59:31 +0300

    Login page complete

---
+++ app/src/main/java/org/mys/mysadmin/ui/Login.java
@@ -0,0 +1,16 @@
+package org.mys.mysadmin.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.mys.mysadmin.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
